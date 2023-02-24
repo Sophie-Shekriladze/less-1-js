154ca688f03ea5d98d76b2dcb8465c97185ce306
Sophie Shekriladze <55558347+Sophie-Shekriladze@users.noreply.github.com>
2023-02-24 19:07:10 +0400

    add 3

---
+++ script.js
@@ -0,0 +1,32 @@
+// დავალება 1
+
+// let number = 100
+// if (number < 50){
+//     console.log('naklebia 50ze');
+// } else if (number > 20){
+//     console.log('metia 20ze');
+// }
+// else{
+//     console.log('error');
+// }
+
+
+// დავალება 2
+
+// let userName = "mariami";
+
+// let result = userName == "mariami" ? true : false;
+
+// console.log(result);
+
+
+//დავალება 3
+
+// let userName = "mariami";
+// switch (userName) {
+//   case "mariami":
+//     console.log(true);
+//     break;
+//   default:
+//     console.log(false);
+// }
